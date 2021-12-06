001ac76c7e3c73667c55337e707ada55f9dba316
kde-rooi <kasderooij@gmail.com>
2021-12-06 09:10:32 +0100

    redo ts

--- ts_implementation/main.ts
+++ ts_implementation/main.ts
@@ -3,10 +3,28 @@ import { stdin as input, stdout as output } from 'process';
 
 const rl = readline.createInterface({ input, output });
 
-const answer = async () => {
-	await rl.question('What do you think of Node.js? ');
-};
+const question1 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('q1 What do you think of Node.js? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`)
+      resolve()
+    })
+  })
+}
 
-console.log(`Thank you for your valuable feedback: ${answer}`);
+const question2 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('q2 What do you think of Node.js? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`)
+      resolve()
+    })
+  })
+}
 
-rl.close();
+const main = async () => {
+  await question1()
+  await question2()
+  rl.close()
+}
+
+main()
---
+++ ts_implementation/test.sh
@@ -0,0 +1,5 @@
+echo "1
+3 4
+0001
+0011
+0110" | .node /ts_implementation/main.js
