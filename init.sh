797db13729106b24c6470fbd38312e5ca477adbd
kas <kasderooij@gmail.com>
2021-12-08 13:36:50 +0100

    finished project

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-cd map_generator && Make;
+cd map_generator && Make
 cd ../ts_implementation
 npm install readline
 npm install process
 npm i --save-dev @types/node
-tsc --project tsconfig.json;
+tsc --project tsconfig.json
